54beef1d30fb09ba19b4b8abb4f9305a1120db5c
Astaroth14B <pranishacharya08@gmail.com>
2026-02-05 21:45:48 +0545

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,15 +1,24 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME || 'ai_art_gallery',
-    process.env.DB_USER || 'root',
-    process.env.DB_PASS || '',
-    {
-        host: process.env.DB_HOST || 'localhost',
+let sequelize;
+
+if (process.env.MYSQL_URL) {
+    sequelize = new Sequelize(process.env.MYSQL_URL, {
         dialect: 'mysql',
         logging: false
-    }
-);
+    });
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME || 'ai_art_gallery',
+        process.env.DB_USER || 'root',
+        process.env.DB_PASS || '',
+        {
+            host: process.env.DB_HOST || 'localhost',
+            dialect: 'mysql',
+            logging: false
+        }
+    );
+}
 
 module.exports = sequelize;
